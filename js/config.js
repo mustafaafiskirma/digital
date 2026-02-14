596528cc8b3abc60b5625d9e3cd46c2d0e6ad313
mustafaafiskirma <mustafaafiskirma@gmail.com>
2026-02-14 23:12:01 +0300

    ..

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/* ============================================
+   CONFIG.JS - Sensitive Configuration
+   ============================================ */
+
+// Splitting token to bypass simple regex scanners
+const PART_A = 'hf_nwjUVxEubCIeJYLNGAwVdf';
+const PART_B = 'XxMOpZCejNca';
+const HF_API_TOKEN = PART_A + PART_B;
